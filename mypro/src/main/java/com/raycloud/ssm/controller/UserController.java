085ebfe9f5b7b6594741b8580996827548c8144a
李琦 <liqi@raycloud.com>
2016-04-06 23:58:06 +0800

    修改something

--- mypro/src/main/java/com/raycloud/ssm/controller/UserController.java
+++ mypro/src/main/java/com/raycloud/ssm/controller/UserController.java
@@ -1,12 +1,12 @@
-package com.raycloud.ssm.controller;
-
+﻿package com.raycloud.ssm.controller;
+//#--------------------------------git
 import com.raycloud.ssm.pojo.User;
 import com.raycloud.ssm.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
+//------------------------git
 import java.util.List;
 
 /**
